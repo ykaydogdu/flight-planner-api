add04cd6cc8e6b3e4b820c27cc59daa2b42beaa6
ykaydogdu <y.kaan.aydogdu@gmail.com>
2025-06-17 14:27:50 +0300

    created global advice class to handle common exceptions gracefully

---
+++ src/main/java/com/flightplanner/api/Advice.java
@@ -0,0 +1,20 @@
+package com.flightplanner.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class Advice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> illegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
