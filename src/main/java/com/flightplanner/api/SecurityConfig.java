16c6a6337ca07e48ef76adaf5603fd70f679425f
ykaydogdu <y.kaan.aydogdu@gmail.com>
2025-06-26 13:52:31 +0300

    move lost resttemplate bean to SecurityConfig.java (previously in H2Config.java)

--- src/main/java/com/flightplanner/api/SecurityConfig.java
+++ src/main/java/com/flightplanner/api/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -99,5 +100,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
